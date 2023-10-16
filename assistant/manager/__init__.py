dcc11dc559e7f2ed8b71fef1436fc155f3c1308b
rendisadia <147367892+rendisadia@users.noreply.github.com>
2023-10-16 19:19:32 +0600

    Add files via upload

---
+++ assistant/manager/__init__.py
@@ -0,0 +1,11 @@
+# Ultroid - UserBot
+# Copyright (C) 2021-2022 TeamUltroid
+#
+# This file is a part of < https://github.com/rendisadia/mujib/ >
+# PLease read the GNU Affero General Public License in
+# <https://github.com/rendisadia/mujibblob/main/LICENSE/>.
+
+from pyUltroid._misc._decorators import ultroid_cmd
+from pyUltroid.fns.helper import inline_mention
+
+from .. import *
